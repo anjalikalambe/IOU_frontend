c2f6f3cb3688c378554a8fa3c568aaebbf4f0310
anjali kalambe <anjalikalambe@outlook.com.au>
2020-10-16 15:54:20 +1100

    Connect requests page to get data from backend

--- src/views/HomeRequests.jsx
+++ src/views/HomeRequests.jsx
@@ -7,73 +7,69 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import axios from "axios";
 import "./HomeLeaderboard.scss";
-function createData(requester, task, reward) {
-  return { requester, task, reward };
-}
-let rows = [
-  createData("David", "Someone wash the dishes please", [
-    "Coffee",
-    "Chocolate",
-  ]),
-  createData("David", "Someone wash the dishes please", [
-    "Coffee",
-    "Chocolate",
-  ]),
-  createData("David", "Someone wash the dishes please", [
-    "Coffee",
-    "Chocolate",
-  ]),
-  createData("David", "Someone wash the dishes please", [
-    "Coffee",
-    "Chocolate",
-  ]),
-  createData("David", "Someone wash the dishes please", [
-    "Coffee",
-    "Chocolate",
-  ]),
-];
 
-const HomeLeaderboard = (props) => {
-  return (
-    <div id="login" class="home-leaderboard">
-      <div className="homenav-wrapper">
-        <HomeNav />
-      </div>
-      <div className="bg" />
-      <div className="table-wrapper">
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Requester</TableCell>
-                <TableCell>Task</TableCell>
-                <TableCell>Rewards</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row, index) => (
-                <TableRow key={row.requester + index}>
-                  <TableCell>{row.requester}</TableCell>
-                  <TableCell>{row.task}</TableCell>
-                  <TableCell>
-                    {row.reward.map((item, index) => {
-                      return (
-                        <span>
-                          {item}
-                          {row.reward.length - 1 !== index && ", "}
-                        </span>
-                      );
-                    })}
-                  </TableCell>
+class HomeRequests extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {rows: []};
+  }
+
+  componentDidMount() {
+    console.log('component rendered');
+
+      axios.get("http://localhost:5000/public/requests/")
+        .then(response => {
+          let requests = response.data;
+          this.setState({ rows: requests });
+        })
+        .catch(e => {
+          console.log("Error: " + e);
+        });
+  }
+
+  render() {
+    return (
+      <div id="login" class="home-leaderboard">
+        <div className="homenav-wrapper">
+          <HomeNav />
+        </div>
+        <div className="bg" />
+        <div className="table-wrapper">
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Requester</TableCell>
+                  <TableCell>Task</TableCell>
+                  <TableCell>Rewards</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {this.state.rows.map((row, index) => (
+                  <TableRow key={row.opened_by + index}>
+                    <TableCell>{row.opened_by}</TableCell>
+                    <TableCell>{row.description}</TableCell>
+                    <TableCell>
+                      {row.rewards.map((reward, index) => {
+                        return (
+                          <span>
+                            {reward.item}
+                            {row.rewards.length - 1 !== index && ", "}
+                          </span>
+                        );
+                      })}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default HomeLeaderboard;
+export default HomeRequests;
